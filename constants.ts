d115e3f32a50375d0721379db6b57fa6f0eef2ad
deipy365@gmail.com <jori3kevin@gmail.com>
2025-11-29 15:18:06 +0800

    apply new image link

--- constants.ts
+++ constants.ts
@@ -1,11 +1,11 @@
 import { Post, Photo } from './types';
 
 export const MOCK_PHOTOS: Photo[] = [
-  { id: '1', url: 'https://etanchan.com/images/DSC00519.jpg', caption: 'Hong Kong' },
-  { id: '2', url: 'https://etanchan.com/images/DSC00455-2.jpg', caption: 'Hong Kong' },
-  { id: '3', url: 'https://etanchan.com/images/DSC00494.jpg', caption: 'Hong Kong' },
-  { id: '4', url: 'https://etanchan.com/images/DSC00524.jpg', caption: 'Hong Kong' },
-  { id: '5', url: 'https://etanchan.com/images/DSC00435-2.jpg', caption: 'Hong Kong' },
-  { id: '6', url: 'https://etanchan.com/images/2.jpg', caption: 'Foshan' },
-  { id: '7', url: 'https://etanchan.com/images/DSC00271.jpg', caption: 'Macau' },
+  { id: '1', url: 'https://etanchan.com/images/gallery/hongkong-2.jpg', caption: 'Hong Kong' },
+  { id: '2', url: 'https://etanchan.com/images/gallery/hongkong-3.jpg', caption: 'Hong Kong' },
+  { id: '3', url: 'https://etanchan.com/images/gallery/hongkong-4.jpg', caption: 'Hong Kong' },
+  { id: '4', url: 'https://etanchan.com/images/gallery/hongkong-5.jpg', caption: 'Hong Kong' },
+  { id: '5', url: 'https://etanchan.com/images/gallery/hongkong-1.jpg', caption: 'Hong Kong' },
+  { id: '6', url: 'https://etanchan.com/images/gallery/foshan-1.jpg', caption: 'Foshan' },
+  { id: '7', url: 'https://etanchan.com/images/gallery/macau-1.jpg', caption: 'Macau' },
 ];
